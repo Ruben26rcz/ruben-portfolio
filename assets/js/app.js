f86f49c6952fdb43b4d25f5af67106775cb300d9
Ruben26rcz <ruben26rcz@gmail.com>
2022-03-17 23:26:46 +0100

    JavaScript Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   $('.js-menu').on('click', function () {
     $('html').toggleClass('menu-open');
   });
 });
+*/
 
-//$('.js-intro__description').delay(800).animate({ 'marginTop': '+=-80px', 'opacity': '1' }, 800);
+function onDocumentReady(callback) {
+  const state = document.readyState;
+  if (state === 'complete' || state === 'interactive') {
+    return callback();
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    callback();
+  });
+}
+
+onDocumentReady(function () {
+  const triggers = document.querySelectorAll('.js-menu');
+
+  triggers.forEach(function (trigger) {
+    trigger.addEventListener('click', function () {
+      document.documentElement.classList.toggle('menu-open');
+    })
+  })
+
+});
